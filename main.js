5d25d938dba0f5c837af0584bc47596091c8fecb
simonestrinati <simone.strinati@outlook.it>
2025-10-31 02:29:54 +0100

    Fine-esercizio

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ console.log("JS Funziona");
 
 //Inizio
 
-for (let i = 0 ; i <= 100; i++) {
-     console.log(i);
-};
+for (let i = 1; i <= 100; i++) {
+     
    
-
 //Svolgimento
 
+if ( i % 15 == 0 ) {
+    console.log("FizzBuzz"); 
+} else if ( i % 5 == 0) {
+    console.log("Buzz");
+} else if ( i % 3 == 0 ) {
+    console.log("Fizz");
+} else {
+    console.log(i);
+}
+
+}
+
 //Output
+
+console.log("Fine del ciclo")
